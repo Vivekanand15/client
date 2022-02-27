feca1686bf8ec15d674566505b83eee28523bfe2
Vivekanand <vivekanand.v@surya-soft.com>
2022-02-27 13:09:38 +0530

    we just see how to set with navigation

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,23 +1,24 @@
 import React  from "react";
 import { BrowserRouter,Route,Routes } from "react-router-dom";
-
-
-const pageOne=()=>{
-    return <h2>Page One</h2>;
-};
-
-const pageTwo=()=>{
-    return <h2>Page Two</h2>;
-};
+import StreamList from "./streams/StreamList";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamShow from "./streams/StreamShow";
+import StreamDelete from "./streams/StreamDelete";
+import Header from "./Header";
 
 
 const App=()=>{
     return (
-        <div>
+        <div className="ui container">
             <BrowserRouter>
+                    <Header  />
                 <Routes>
-                    <Route path="/" exact component={pageOne} ></Route>
-                    <Route path="/pagetwo" component={pageTwo} ></Route>
+                    <Route path="/" element={< StreamList />} />
+                    <Route path="/stream/new" element={ < StreamCreate />} />
+                    <Route path="/stream/edit" element={<StreamEdit />} />
+                    <Route path="/stream/delete" element={<StreamDelete />} />
+                    <Route path="/stream/show" element={<StreamShow />} />
                 </Routes>
             </BrowserRouter>
         </div>
